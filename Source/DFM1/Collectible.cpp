1e33d51f8d3237de6b61e42d962700087af60af7
NysoS <nwanysos79@gmail.com>
2023-03-05 19:27:36 +0100

    #27 adding doc into readme

--- Source/DFM1/Collectible.cpp
+++ Source/DFM1/Collectible.cpp
@@ -11,7 +11,7 @@
 ACollectible::ACollectible()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	SphereCollider = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Collider"));
 	RootComponent = SphereCollider;
--- Source/DFM1/EndingZone.cpp
+++ Source/DFM1/EndingZone.cpp
@@ -12,7 +12,7 @@
 AEndingZone::AEndingZone()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Trigger Zone"));
 	RootComponent = BoxComponent;
